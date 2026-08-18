52c9c5cd31dc1b412c70c71583d6a764687a0bbd
Ayan Mondal <ayanmondal862@gmail.com>
2026-08-18 12:22:31 +0530

    first commit

---
+++ models/chat.js
@@ -0,0 +1,26 @@
+const mongoose  = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    from : {
+        type: String,
+        require: true,
+    },
+    to: {
+        type: String,
+        require: true,
+    },
+    msg: {
+        type: String,
+    },
+    created_at: {
+        type: Date,
+        require: true,
+    },
+    updated_at: {
+        type: Date,
+    }
+});
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+module.exports = Chat;
